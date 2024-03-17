8fb15283c2ef1a6da8c773d5194424d6538e3c46
LizbethVidal <lunishka23@gmail.com>
2024-03-17 23:58:10 +0000

    Actualizar diseño de la página de edición de médicos

--- resources/views/modules/medicos/edit.blade.php
+++ resources/views/modules/medicos/edit.blade.php
@@ -17,7 +17,7 @@
                         @csrf
                         @method('PUT')
                         <div class="row">
-                            <div class="col-4">
+                            <div class="col-md-4">
                                 <div class="mt-2 profile_img">
                                     <img id="preview" src="{{ $user->foto ? asset('storage/' . $user->foto) : '/images/default.png' }}" alt="Foto de Perfil" class="img-thumbnail" style="max-width: 200px;">
                                 </div>
@@ -31,10 +31,10 @@
                                 </div>
                             </div>
 
-                            <div class="col-12 mb-3"></div>
+                            <div class="col-md-12 mb-3"></div>
                             <hr>
 
-                            <div class="col-6">
+                            <div class="col-md-6">
                                 <label for="name">Nombre</label>
                                 <input type="text" name="name" id="name" class="form-control @if($errors->has('name')) is-invalid @endif" placeholder="Nombre" required value="{{$user->name}}">
                                 @if($errors->has('name'))
@@ -43,7 +43,7 @@
                                     </span>
                                 @endif
                             </div>
-                            <div class="col-6">
+                            <div class="col-md-6">
                                 <label for="email">Correo</label>
                                 <input type="email" name="email" id="email" class="form-control @if($errors->has('email')) is-invalid @endif" placeholder="Correo" required value="{{$user->email}}">
                                 @if($errors->has('email'))
@@ -53,7 +53,7 @@
                                 @endif
                             </div>
 
-                            <div class="col-3">
+                            <div class="col-md-3">
                                 <label for="dni">DNI</label>
                                 <input type="text" name="dni" id="dni" class="form-control @if($errors->has('dni')) is-invalid @endif" placeholder="DNI" required value="{{$user->dni}}">
                                 @if($errors->has('dni'))
@@ -62,7 +62,7 @@
                                     </span>
                                 @endif
                             </div>
-                            <div class="col-3">
+                            <div class="col-md-3">
                                 <label for="movil">Teléfono</label>
                                 <input type="text" name="movil" id="movil" class="form-control @if($errors->has('movil')) is-invalid @endif" placeholder="Teléfono" required value="{{$user->movil}}">
                                 @if($errors->has('movil'))
@@ -71,7 +71,7 @@
                                     </span>
                                 @endif
                             </div>
-                            <div class="col-3">
+                            <div class="col-md-3">
                                 <label for="fecha_nac">Fecha de Nacimiento</label>
                                 <input type="date" name="fecha_nac" id="fecha_nac" class="form-control @if($errors->has('fecha_nac')) is-invalid @endif" placeholder="Fecha de Nacimiento" value="{{$user->fecha_nac}}">
                                 @if($errors->has('fecha_nac'))
@@ -80,7 +80,7 @@
                                     </span>
                                 @endif
                             </div>
-                            <div class="col-3">
+                            <div class="col-md-3">
                                 <label for="especialidad_id">Especialidad</label>
                                 <select name="especialidad_id" id="especialidad_id" class="form-control form-select @if($errors->has('especialidad_id')) is-invalid @endif" required>
                                     <option value="">Seleccione una especialidad</option>
@@ -89,11 +89,11 @@
                                     @endforeach
                                 </select>
                             </div>
-                            <div class="col-9">
+                            <div class="col-md-9">
                                 <label for="direccion">Dirección</label>
                                 <input type="text" name="direccion" id="direccion" class="form-control @if($errors->has('direccion')) is-invalid @endif" placeholder="Dirección" value="{{$user->direccion}}">
                             </div>
-                            <div class="col-12 mt-3">
+                            <div class="col-md-12 mt-3">
                                 <a href="{{route('empleados.index')}}" class="btn btn-outline-dark">
                                     <i class="bi bi-arrow-left-circle"></i> Volver
                                 </a>
